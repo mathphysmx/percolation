1bee2cac512038452a145c78b1640561583762ae
Francisco Mendoza Torres <mentofran@gmail.com>
2019-08-12 22:13:39 -0400

    Line segment representation conversion

--- R/lineSegmentsCRSConversion.R
+++ R/lineSegmentsCRSConversion.R
@@ -1,4 +1,4 @@
-#' @title Line segments conversion
+#' @title Line segments Coordinate Reference System conversion
 #' @description Line segments conversion from a angles-length-centers to endpoints coordinates
 # c is the x position of the center of the line segment
 # c is the y position of the center of the line segment
@@ -6,25 +6,24 @@
 # l is the length of the line segment
 #' @references  equation 1.78, page 56 of D:/IMP/Transferencia/ReporteMetodologiasDFN_FmendozaT.pdf
 #' @examples 
-# ae <- circular(c(0, 45, 90, 135),units="degrees",
-#                template="geographics")
-# le <- c(2, sqrt(2) * 3, 3, sqrt(2) * 3)
-# ends <- data.frame(x0 = c(0, 1, 1, -1),
-#                    y0 = c(1, 1, 0,  1),
-#                    x1 = c(0, 4, 4, -4),
-#                    y1 = c(3, 4, 0,  4)
-# )
-# xe <- data.frame(a = ae, l = le,
-#                    x = (ends$x0 + ends$x1) / 2,
-#                    y = (ends$y0 + ends$y1) / 2) 
-# PlotSegments(
-#   segment=list( g1 = list(x0 = ends$x0, y0 = ends$y0,
-#                           x1 = ends$x1, y1 = ends$y1)),
-#   asp=1, main="Segments")
-# points.default(xe$x, xe$y, pch=20) # adding the midpoints to the current plot
-# print(ends)
-# lineSegmentsCRSConversion(x = xe)
-
+#' ae <- circular(c(0, 45, 90, 135),units="degrees",
+#'                template="geographics")
+#' le <- c(2, sqrt(2) * 3, 3, sqrt(2) * 3)
+#' ends <- data.frame(x0 = c(0, 1, 1, -1),
+#'                    y0 = c(1, 1, 0,  1),
+#'                    x1 = c(0, 4, 4, -4),
+#'                    y1 = c(3, 4, 0,  4)
+#' )
+#' xe <- data.frame(a = ae, l = le,
+#'                    x = (ends$x0 + ends$x1) / 2,
+#'                    y = (ends$y0 + ends$y1) / 2) 
+#' plotSegments(
+#'   segment=list( g1 = list(x0 = ends$x0, y0 = ends$y0,
+#'                           x1 = ends$x1, y1 = ends$y1)),
+#'   asp=1, main="Segments")
+#' points.default(xe$x, xe$y, pch=20) # adding the midpoints to the current plot
+#' print(ends)
+#' lineSegmentsCRSConversion(x = xe)
 lineSegmentsCRSConversion <- function(x){
   
   # From geographic to mathemical coordinate system
