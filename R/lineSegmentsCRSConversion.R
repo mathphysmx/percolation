a41f0d728cccc8cc169c65e72f03bc80f86ad5ca
Francisco Mendoza-Torres <mentofran@gmail.com>
2019-08-13 00:05:50 -0400

    Updating documentation

--- R/lineSegmentsCRSConversion.R
+++ R/lineSegmentsCRSConversion.R
@@ -1,11 +1,11 @@
 #' @title Line segments Coordinate Reference System conversion
 #' @description Line segments conversion from a angles-length-centers to endpoints coordinates
-# c is the x position of the center of the line segment
-# c is the y position of the center of the line segment
-# a object of class circular (circular package).It is the  of the line segment 
+# x x coordinate of the center of the line segment
+# y y coordinate of the center of the line segment
+# a object of class \link[circular]{circular} (circular package).It is the  of the line segment
 # l is the length of the line segment
 #' @references  equation 1.78, page 56 of D:/IMP/Transferencia/ReporteMetodologiasDFN_FmendozaT.pdf
-#' @examples 
+#' @examples
 #' ae <- circular(c(0, 45, 90, 135),units="degrees",
 #'                template="geographics")
 #' le <- c(2, sqrt(2) * 3, 3, sqrt(2) * 3)
@@ -16,7 +16,7 @@
 #' )
 #' xe <- data.frame(a = ae, l = le,
 #'                    x = (ends$x0 + ends$x1) / 2,
-#'                    y = (ends$y0 + ends$y1) / 2) 
+#'                    y = (ends$y0 + ends$y1) / 2)
 #' plotSegments(
 #'   segment=list( g1 = list(x0 = ends$x0, y0 = ends$y0,
 #'                           x1 = ends$x1, y1 = ends$y1)),
@@ -25,7 +25,7 @@
 #' print(ends)
 #' lineSegmentsCRSConversion(x = xe)
 lineSegmentsCRSConversion <- function(x){
-  
+
   # From geographic to mathematical coordinate system
   a_rad <- conversion.circular(x = x$a, type = "angles",
                                        units = "radians", modulo = "pi",
@@ -37,7 +37,7 @@ lineSegmentsCRSConversion <- function(x){
   x1 <- x$x +           rs
   y0 <- x$y - (slope) * rs
   y1 <- x$y + (slope) * rs
-  
+
   return(list(df = data.frame(x0, y0, x1, y1),
               ls = list(x0 = x0, y0 = y0,
                         x1 = x1, y1 = y1)))
